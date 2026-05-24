030a526eb0be8c14d59e1cb92b5649c31690c40b
avatiah <106751153+avatiah@users.noreply.github.com>
2026-05-24 09:18:17 +0300

    Fix import statement for ProductRecord

--- sources/base.py
+++ sources/base.py
@@ -1,10 +1,10 @@
 from abc import ABC, abstractmethod
 from typing import List
-from models import ProductRecord
+from .models import ProductRecord
 
 class BaseSource(ABC):
     name: str
 
     @abstractmethod
     def fetch(self, keyword: str) -> List[ProductRecord]:
-        raise NotImplementedError
+        pass
